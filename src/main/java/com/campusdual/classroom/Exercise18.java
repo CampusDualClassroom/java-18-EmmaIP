5ba664ab8f090b7224f5e6192d51836a0bf3882e
EmmaIP <emmapineiro@gmail.com>
2024-10-21 12:43:59 +0200

    ejercicio 18 finalizado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,31 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
 public class Exercise18 {
 
+	public static int[] createAndInitializeArray(int number) {
+		int [] array = new int[number];
+		for(int i=0; i <array.length; i++) {
+			array[i] = i + 1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array) {
+		for(int i=0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if(i < array.length -1) {
+				System.out.print(" ");
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+
+		int number = Utils.integer("Write a number: ");
+		createAndInitializeArray(number);
+		showInlineArray(createAndInitializeArray(number));
 
 	}
 }
